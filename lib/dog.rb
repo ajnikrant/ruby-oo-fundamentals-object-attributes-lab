152e00d03c90694d1550079983f10cd3b7d17391
Amanda <ajnikrant@gmail.com>
2021-02-22 19:15:08 -0500

    commit tree eed93f0734ecdf3b02203ad70bef444cb99cf8af

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog    
+      def name=(new_dogs_name)
+        @name = new_dogs_name
+      end
+
+      def breed=(new_dogs_breed)
+        @breed = new_dogs_breed
+      end
+    
+      def name
+        @name
+      end
+      def breed
+        @breed
+      end 
+end
